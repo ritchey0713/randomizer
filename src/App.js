6b27e074bbb07d67150a226ff6d8420463741781
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-19 21:31:21 -0400

    stub out container class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+// leaving app intact for scalability in the future is not needed currently
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        
       </div>
     );
   }
