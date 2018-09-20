859d9aeac06e1394fdce2fefbc6695508286576d
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-19 21:49:25 -0400

    create add handler

--- src/containers/randomizer.js
+++ src/containers/randomizer.js
@@ -6,6 +6,24 @@ class Randomizer extends Component {
   state = {
     options = []
   }
+
+  handleAddOptionEvent = (option) => {
+    // option must be valid JS object
+    if (!option){
+      return "Enter valid option please!"
+    }
+    //option test, if it has a index better than -1 it is already in array optionally
+    // could have called a built in function to test array this seemed to be a stronger way 
+    else if (this.state.options.indexOf(option) > -1){
+      return "This option already exists!"
+    }
+
+    //if it doesn't return before here it is a valid option and no need for else statement
+    this.setState((prevState) => {
+      ({options: prevState.options.concat([option])})
+    })
+  }
+
   render(){
     return (
 
