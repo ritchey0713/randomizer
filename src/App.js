3c8e7df5ba9cc446a5ca5c5691499ed6ffe8b5ba
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-19 21:38:16 -0400

    call randomizerr from app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Randomizer from './containers/randomizer.js'
 
 import './App.css';
 // leaving app intact for scalability in the future is not needed currently
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Randomizer />
       </div>
     );
   }
