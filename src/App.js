83a0dff5626666f3c7779d8f51e21e11dfb26089
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-08 17:50:47 -0400

    add correct routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import React, { Component } from 'react';
-import Randomizer from './containers/randomizer.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import Randomizer from './components/randomizer';
 
-import './App.css';
-// leaving app intact for scalability in the future is not needed currently
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Randomizer className="main" />
-      </div>
-    );
-  }
-}
-
-export default App;
+ReactDOM.render(<Randomizer />, document.getElementById('app'));
